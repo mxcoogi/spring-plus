6d3de417e71ca343bad1e043d7ceffb6cbf0b00c
mxcoogi <mxcoogi@gmail.com>
2025-05-11 22:40:01 +0900

    lv3-12 : AWS활용 ping API

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.service.AuthService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,9 @@ public class AuthController {
     @PostMapping("/auth/signin")
     public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
         return authService.signin(signinRequest);
+    }
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
     }
 }
